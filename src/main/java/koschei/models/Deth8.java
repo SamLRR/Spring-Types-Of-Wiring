2177b2a91e095674fcfc307d17cc8e114cbf5b3f
SamLRR <samatov_lr@mail.ru>
2021-09-06 09:32:59 +0300

    Add @Autowiring annotations for Rabbit4, Duck5, Egg6, Needle7, Deth8 classes

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Deth8 {
+
+    @Autowired
+    public Deth8() {
+    }
+
+    @Override
+    public String toString() {
+        return "Сказке конец)";
+    }
+}
